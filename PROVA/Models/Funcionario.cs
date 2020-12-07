4862e261a573b65ccaeea4e999632970e162eac0
caio <caio.ferraz@souunit.com.br>
2020-12-07 16:12:35 -0300

    Adicionar arquivos de projeto.

---
+++ PROVA/Models/Funcionario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PROVA.Models
+{
+    public class Funcionario
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public int matricula { get; set; }
+
+        public double salario { get; set; }
+    }
+}
